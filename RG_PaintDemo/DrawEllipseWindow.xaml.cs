2389594a11ccd5676fd28ea7fd4aeae4e3cb2ccd
Marko Sutic <msuticm@gmail.com>
2021-03-15 19:22:37 +0100

    draw ellipse window created

--- RG_PaintDemo/DrawEllipseWindow.xaml.cs
+++ RG_PaintDemo/DrawEllipseWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RG_PaintDemo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DrawEllipse.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DrawEllipse : Window
     {
-        public MainWindow()
+        public DrawEllipse()
         {
             InitializeComponent();
         }
-
-        private void EllipseButton_Click(object sender, RoutedEventArgs e)
-        {
-            DrawEllipse ellipseWindow = new DrawEllipse();
-            ellipseWindow.Show();
-        }
-    
     }
 }
