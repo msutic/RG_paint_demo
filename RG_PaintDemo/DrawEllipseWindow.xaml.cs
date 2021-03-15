e5fac0ebad9db4596b7f240fdff180407365795d
Marko Sutic <msuticm@gmail.com>
2021-03-15 19:58:49 +0100

    validation for DrawEllipse window

--- RG_PaintDemo/DrawEllipseWindow.xaml.cs
+++ RG_PaintDemo/DrawEllipseWindow.xaml.cs
@@ -23,5 +23,81 @@ namespace RG_PaintDemo
         {
             InitializeComponent();
         }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public bool validate()
+        { 
+            if (widthIn.Text.Trim().Equals("") || int.Parse(widthIn.Text) < 10)
+            {
+                widthIn.BorderBrush = Brushes.Red;
+                widthIn.BorderThickness = new Thickness(1);
+                MessageBox.Show("Width must be at least 10.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            {
+                widthIn.BorderBrush = Brushes.DarkGray;
+            }
+
+            if (heightIn.Text.Trim().Equals("") || int.Parse(heightIn.Text) < 10)
+            {
+                heightIn.BorderBrush = Brushes.Red;
+                heightIn.BorderThickness = new Thickness(1);
+                MessageBox.Show("Height must be at least 10.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else
+            {
+                widthIn.BorderBrush = Brushes.DarkGray;
+            }
+
+            if (fillColorIn.SelectedColor == null)
+            {
+                fillColorIn.BorderBrush = Brushes.Red;
+                fillColorIn.BorderThickness = new Thickness(1);
+                MessageBox.Show("Fill color must be selected.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else
+            {
+                widthIn.BorderBrush = Brushes.DarkGray;
+            }
+
+            if (borderColorIn.SelectedColor == null)
+            {
+                borderColorIn.BorderBrush = Brushes.Red;
+                borderColorIn.BorderThickness = new Thickness(1);
+                MessageBox.Show("Border color must be selected.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            } else
+            {
+                widthIn.BorderBrush = Brushes.DarkGray;
+            }
+
+            if (borderThicknessIn.Text.Trim().Equals("") || int.Parse(borderThicknessIn.Text) < 1)
+            {
+                borderThicknessIn.BorderBrush = Brushes.Red;
+                borderThicknessIn.BorderThickness = new Thickness(1);
+                MessageBox.Show("Border thickness must be at least 1.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            } else
+            {
+                widthIn.BorderBrush = Brushes.DarkGray;
+            }
+
+            return true;
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (validate())
+            {
+
+            }
+                
+        }
     }
 }
